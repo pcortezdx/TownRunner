70d9e1a1169638863390714e495a2d8f9fc1b506
PatCR <pcortez.dx@gmail.com>
2023-03-11 23:56:39 +0900

    Initializing the project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float forceUpValue = 10f;
+    public float gravityModifier = 1f;
+    public bool isOnGround = true;
+
+    void Start()
+    {
+        /**Because RigidBody is not a component that comes  
+         * in every GameObject like the Transform, instead is a 
+         * component that we add.Therefore, we need to 
+         * use GetComponent<Component_type> 
+        **/
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
+    }
+
+
+    void Update()
+    {
+        // Add an instant force (impulse) to move the player
+        // when the user hits the spacebar.
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * forceUpValue, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}

83b6fa9a161ec5c92988937caaf6a51c8c6ff17c
PatCR <pcortez.dx@gmail.com>
2023-03-18 22:45:16 +0900

    Adding Run, Jump, an Death player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    
+    private Animator playerAnimator;
+
     public float forceUpValue = 10f;
     public float gravityModifier = 1f;
     public bool isOnGround = true;
@@ -20,17 +21,24 @@ public class PlayerController : MonoBehaviour
         **/
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-        
+
+        // Getting the animation component attached to the player
+        playerAnimator = GetComponent<Animator>();
+
     }
 
     void Update()
     {
         // Add an instant force (impulse) to move the player
         // when the user hits the spacebar.
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * forceUpValue, ForceMode.Impulse);
             isOnGround = false;
+
+            //The animator parameters Jump_trig controls is used to
+            // trigger the jump animation.
+            playerAnimator.SetTrigger("Jump_trig");
         }
     }
 
@@ -44,6 +52,10 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+
+            //Changing parameters for Death animation
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
         }
         
     }
